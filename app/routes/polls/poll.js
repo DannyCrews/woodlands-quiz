24421e81a4cf925266ee8b5f805eb804e495d6b7
dannycrews <dancrews@me.com>
2016-08-07 16:56:35 -0400

    Add forms and actions for question submittal and voting

--- app/routes/polls/poll.js
+++ app/routes/polls/poll.js
@@ -1,11 +1,16 @@
 import Ember from 'ember';
 
 export default Ember.Route.extend({
-  model(params) {
-    const store = this.get('store');
-    return store.getPollById(params.poll_id);
+  actions: {
+    voteForOption(poll, option) {
+      option.incrementProperty('votes');
+      this.transitionTo('polls.results', poll);
+    }
   },
-  
-  
-  store: Ember.inject.service('store')
+
+  model(params) {
+    return this.get('store').getPollById(params.poll_id);
+  },
+
+  store: Ember.inject.service()
 });
--- app/services/store.js
+++ app/services/store.js
@@ -1,15 +1,15 @@
 import Ember from 'ember';
-import Poll from '../models/poll';
-import Option from '../models/option';
+import Option from 'quiz/models/option';
+import Poll from 'quiz/models/poll';
 
 const polls = [
   Poll.create({
     id: '1',
     question: 'Which Poisonous Plant Are You?',
     options: [
-      Option.create({ id: '1', value: 'Nightshade' }),
-      Option.create({ id: '2', value: 'Hemlock' }),
-      Option.create({ id: '3', value: 'Rhubarb' })
+      Option.create({ id: '1', value: 'Nightshade', votes: 1 }),
+      Option.create({ id: '2', value: 'Hemlock', votes: 5 }),
+      Option.create({ id: '3', value: 'Rhubarb', votes: 0 })
     ]
   }),
 
@@ -17,20 +17,35 @@ const polls = [
     id: '2',
     question: 'Which Is Your Favorite Woodland Wanderer Way?',
     options: [
-      Option.create({ id: '4', value: 'Honesty' }),
-      Option.create({ id: '5', value: 'Integrity' }),
-      Option.create({ id: '6', value: 'Patience' })
+      Option.create({ id: '4', value: 'Honesty', votes: 3 }),
+      Option.create({ id: '5', value: 'Integrity', votes: 4 }),
+      Option.create({ id: '6', value: 'Patience', votes: 2 })
     ]
   })
 ];
 
 export default Ember.Service.extend({
-  getPollById(id) {
-    const polls = this.getPolls();
-    return polls.findBy('id', id);
+  createPoll(poll) {
+    poll.set('id', (polls.length + 1).toString());
+    polls.pushObject(poll);
+    return poll;
   },
-  
+
+  getPollById(id) {
+    return this.getPolls().findBy('id', id);
+  },
+
   getPolls() {
     return polls;
+  },
+
+  newPoll() {
+    return Poll.create({
+      options: [
+        Option.create({ votes: 0 }),
+        Option.create({ votes: 0 }),
+        Option.create({ votes: 0 })
+      ]
+    });
   }
 });
