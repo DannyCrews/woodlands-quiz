0c76fc131ec0237a3283e126e846501580c7307e
dannycrews <dancrews@me.com>
2016-08-07 16:05:28 -0400

    Fill out store service and update models

--- app/services/store.js
+++ app/services/store.js
@@ -1,4 +1,36 @@
 import Ember from 'ember';
+import Poll from '../models/poll';
+import Option from '../models/option';
+
+const polls = [
+  Poll.create({
+    id: '1',
+    question: 'Which Poisonous Plant Are You?',
+    options: [
+      Option.create({ id: '1', value: 'Nightshade' }),
+      Option.create({ id: '2', value: 'Hemlock' }),
+      Option.create({ id: '3', value: 'Rhubarb' })
+    ]
+  }),
+
+  Poll.create({
+    id: '2',
+    question: 'Which Is Your Favorite Woodland Wanderer Way?',
+    options: [
+      Option.create({ id: '4', value: 'Honesty' }),
+      Option.create({ id: '5', value: 'Integrity' }),
+      Option.create({ id: '6', value: 'Patience' })
+    ]
+  })
+];
 
 export default Ember.Service.extend({
+  getPollById(id) {
+    const polls = this.getPolls();
+    return polls.findBy('id', id);
+  },
+  
+  getPolls() {
+    return polls;
+  }
 });
